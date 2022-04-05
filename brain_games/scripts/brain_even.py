a1bdbeb052d83f0724cf6df586dbc0f482e3d132
Vitaliy Malyshev <vitatomchyk@gmail.com>
2022-04-05 19:34:17 +0200

    my commit

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,34 +11,25 @@ def welcome_user():
 welcome_user()
 
 
-question_made = 1
-
-
 def question():
-    num = randint(1,99)
-    even = (num % 2) == 0
-    print(f'Question: {num}')
-    player_answer = prompt.string('Your answer: ')
-    if even:
-        if player_answer == str('yes'):
-            print('Correct!')
-            if question_made < 3:
+    question_made = 0
+    while question_made < 3:
+        num = randint(1,99)
+        even = (num % 2) == 0
+        print(f'Question: {num}')
+        player_answer = prompt.string('Your answer: ')
+        if even:
+            if player_answer == str('yes'):
+                print('Correct!')
                 question_made = question_made + 1
-                print(question_made)
-                return question
             else:
-                return (f'Congratulations, {name}!')
+                return print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again {name}!")
         else:
-            return print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again {name}!")
+            if player_answer == str('no'):
+                print('Correct!')
+                question_made = question_made + 1
+            else:
+                return print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again {name}!")
     else:
-        if player_answer == str('no'):
-            print('Correct!')
-            if question_made < 3:
-                question_made = question_made + 1
-                print(question_made)
-                return question
-            else:
-                return (f'Congratulations, {name}!')
-        else:
-            return print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again {name}!")
+        return print(f'Congratulations, {name}!')
 question()
