1ee3a43341c74dd9d33faf91d1f113a943c53b67
Vitaliy Malyshev <vitatomchyk@gmail.com>
2022-04-08 19:54:07 +0200

    my commit

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,5 +1,7 @@
-from brain_games.scripts.games_logic import answer_getter, rules, victory, welcome_user, checker
+from brain_games.scripts.games_logic import (answer_getter, rules, victory,
+                                             welcome_user, checker)
 from random import randint
+
 
 def number_creator():
     fnum = randint(2, 100)
@@ -7,11 +9,13 @@ def number_creator():
     question = (f'{fnum} {snum}')
     return fnum, snum, question
 
+
 def gcd(fnum, snum):
     while snum:
-        fnum, snum = snum, fnum%snum
+        fnum, snum = snum, fnum % snum
     correct_answer = str(fnum)
     return correct_answer
+
 
 def gameplay():
     name = welcome_user()
@@ -21,7 +25,7 @@ def gameplay():
         nums = number_creator()
         correct_answer = gcd(nums[0], nums[1])
         user_answer = answer_getter(nums[2])
-        if checker(correct_answer , user_answer):
+        if checker(correct_answer, user_answer):
             print('Correct!')
             i = i + 1
         else:
@@ -29,8 +33,10 @@ def gameplay():
 Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
     return victory(name)
 
+
 def main():
     gameplay()
+
 
 if __name__ == '__main__':
     main()
